7928df730b319855c2f5316b538facb7d7fbc4a0
ilya <i.bolgofff@yandex.ru>
2022-10-28 16:31:52 +0300

    Login page tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,4 @@
 from .pages.main_page import MainPage
-import time
 
 link = 'http://selenium1py.pythonanywhere.com/'
 
@@ -14,4 +13,7 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+
 
