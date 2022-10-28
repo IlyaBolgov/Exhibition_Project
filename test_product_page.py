f24377debba28be423370c1f1d78d21cf1749d8a
ilya <i.bolgofff@yandex.ru>
2022-10-28 17:20:03 +0300

    First test ProductPage done

---
+++ test_product_page.py
@@ -0,0 +1,21 @@
+from .pages.product_page import ProductPage
+
+link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+
+
+def test_add_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+
+
+# def test_add_to_basket_right_name(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     pass
+
+
+# def test_add_to_basket_right_cost(browser):
+#     page = ProductPage(browser, link)
+#     page.open()
+#     pass
