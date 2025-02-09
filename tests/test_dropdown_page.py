c4d0a83fbbf1227ee4e721437e60297a683e67c3
i.bolgov <bolgov.ilya19@gmail.com>
2025-02-09 20:01:34 +0300

    dropdown page tests added

---
+++ tests/test_dropdown_page.py
@@ -0,0 +1,12 @@
+from pages.dropdown_page import DropdownPage
+
+def test_dropdown_selection(driver):
+    dropdown_page = DropdownPage(driver)
+    dropdown_page.open()
+    dropdown_page.select_option_by_value("1")
+    selected = dropdown_page.get_selected_option_text()
+    assert "Option 1" in selected, "Dropdown should selected Option 1"
+
+    dropdown_page.select_option_by_value("2")
+    selected = dropdown_page.get_selected_option_text()
+    assert "Option 2" in selected, "Dropdown should selected Option 2"
