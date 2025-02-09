c133f758d5133aebc4e0d1ffdd2beff799438d5b
i.bolgov <bolgov.ilya19@gmail.com>
2025-02-09 19:54:37 +0300

    test_input_page added

---
+++ tests/test_input_page.py
@@ -0,0 +1,9 @@
+from pages.inputs_page import InputsPage
+
+def test_input_field(driver):
+    inputs_page = InputsPage(driver)
+    inputs_page.open()
+    test_value = "12345"
+    inputs_page.set_input_value(test_value)
+    value = inputs_page.get_input_value()
+    assert value == test_value, f"Expected value {test_value} but {value}"
